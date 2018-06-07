ec8a9e0554ff7908a2c0436907ebc7f1eb454e2d
LiuJuan69 <115247188759@163.co>
2018-06-07 15:13:08 +0800

    项目初始状态

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data(){
+    return{
+      msg:'欢迎来到菜鸟教程'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
